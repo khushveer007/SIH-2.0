f6cdc1dc01400254d3e48a9d95b3745cc63398e5
khushveer007 <khushveersinghdeora67@gmail.com>
2025-09-03 04:38:57 +0530

    chore: update package.json to include React and Tailwind CSS dependencies

---
+++ frontend/src/components/TestAlias.tsx
@@ -0,0 +1,9 @@
+export function TestAlias() {
+  return (
+    <div style={{ padding: '0.5rem', border: '1px dashed #888', marginTop: '1rem' }}>
+      <strong>Alias Component Loaded:</strong> This component was imported via the '@' alias.
+    </div>
+  );
+}
+
+export default TestAlias;
